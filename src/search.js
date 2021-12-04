446eeba9e52654bc3ad514b513aba3a8e7c6c524
Alenygam <alessandro@alenygam.com>
2021-12-04 17:49:45 +0100

    Removed deprecated non-geo search and added search with language

--- src/search.js
+++ src/search.js
@@ -1,29 +1,25 @@
 import express from 'express';
-import Fuse from 'fuse.js';
-import fs from 'fs';
 import Geode from 'geode';
 
 const router = express.Router();
 
+router.get('/geo/:q', search);
+router.get('/geo/:q/:lang', search);
 
-// TO BE DEPRECATED
-const cities = JSON.parse(fs.readFileSync('./city.list.json', { encoding: 'UTF-8' }));
-const fuse = new Fuse(cities, { keys: ['name', 'country', 'id'] })
-router.get('/:q', (req, res) => {
+function search(req, res) {
   if (!req.params.q) return res.status(400).json({ message: "specifica una stringa di ricerca" });
-  const result = fuse.search(req.params.q).splice(0, 10);
-  res.json(result);
-})
-// END OF DEPRECATED
-
-router.get('/geo/:q', (req, res) => {
-  if (!req.params.q) return res.status(400).json({ message: "specifica una stringa di ricerca" });
-  // Here because we're later going to accept other languages
-  const geo = new Geode('alenygam', {language: 'it'});
-  geo.search({q: req.params.q, maxRows: 10, cities: 'cities500'}, (err, results) => {
-    if (err) return res.status(400).json({message: err.message});
+  if (!req.params.lang) req.params.lang = "it";
+  var geo;
+  try {
+    geo = new Geode('alenygam', { language: req.params.lang });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({message: err.message});
+  }
+  geo.search({ q: req.params.q, maxRows: 10, cities: 'cities500' }, (err, results) => {
+    if (err) return res.status(400).json({ message: err.message });
     res.json(results.geonames);
   })
-})
+}
 
 export default router;
